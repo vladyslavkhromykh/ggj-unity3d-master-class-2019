b4423f7a177aad2eb445a19cb6f10f7b503c9c35
Vladyslav Khromykh <hromyhvladislav@gmail.com>
2019-01-02 00:41:08 +0200

    Added copies of prefabs to resources and sounds for shooting

--- Assets/Scripts/ToySpawner.cs
+++ Assets/Scripts/ToySpawner.cs
@@ -2,5 +2,31 @@
 
 public class ToySpawner : MonoBehaviour
 {
+    [Range(1.0f, 5.0f)]
+    public float SpawnFrequency;
+    public Transform SpawnPlace;
+    public Transform SpawnerDirection;
+    private GameObject[] toyPrefabs;
+    private float spawnTimer;
 
+    private void Awake()
+    {
+        toyPrefabs = Resources.LoadAll<GameObject>("Toys");
+    }
+
+    private void Update()
+    {
+        spawnTimer += Time.deltaTime;
+        if (spawnTimer > SpawnFrequency)
+        {
+            spawnTimer = 0.0f;
+            Spawn();
+        }
+    }
+
+    private void Spawn()
+    {
+        GameObject toy = Instantiate<GameObject>(toyPrefabs[Random.Range(0, toyPrefabs.Length)], SpawnPlace.position, Random.rotation);
+        toy.GetComponent<Rigidbody>().AddForce(Vector3.up * Random.Range(10.0f, 40.0f), ForceMode.Impulse);
+    }
 }
