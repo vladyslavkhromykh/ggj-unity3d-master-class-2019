8155a21aa135c8abf42a7fa1594a39c920369c6b
Vladyslav Khromykh <hromyhvladislav@gmail.com>
2019-01-01 19:26:40 +0200

    Added Gun scipt for basic shooting. Added default-bullet prefab

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    private GameObject bulletPrefab;
+    [Range(5.0f, 100.0f)]
+    public float ShootForce;
+
+    private void Awake()
+    {
+        bulletPrefab = Resources.Load<GameObject>("Bullets/default-bullet");
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        GameObject bullet = Instantiate<GameObject>(bulletPrefab);
+        bullet.transform.position = transform.position + transform.forward * 2.0f;
+        bullet.GetComponent<Rigidbody>().AddForce(transform.forward * ShootForce, ForceMode.Impulse);
+    }
+}
