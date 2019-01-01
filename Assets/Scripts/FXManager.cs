1a2b8c04abcec20d003b439ee1e156fb59acc37f
Vladyslav Khromykh <hromyhvladislav@gmail.com>
2019-01-01 22:52:20 +0200

    Added skybox

---
+++ Assets/Scripts/FXManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class FXManager
+{
+    private static GameObject toyHitFXPrefab;
+
+    public static void Initialize()
+    {
+        toyHitFXPrefab = Resources.Load<GameObject>("FX/ToyHitFX");
+        EventsManager.BulletHitToy += OnBulletHitToy;
+    }
+
+    private static void OnBulletHitToy(Toy toy)
+    {
+        Vector3 toyPosition = toy.transform.position;
+        GameObject toyHitFX = GameObject.Instantiate<GameObject>(toyHitFXPrefab);
+        toyHitFX.transform.position = toyPosition;
+    }
+}
