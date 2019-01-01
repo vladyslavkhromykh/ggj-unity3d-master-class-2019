74703eb45fba88acbee2a0049ef5104f565fe3e9
Vladyslav Khromykh <hromyhvladislav@gmail.com>
2019-01-01 20:06:37 +0200

    Added camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Range(1.0f, 10.0f)]
+    public float LookSpeed;
+    private Vector2 rotation = Vector2.zero;
+
+    private void Update()
+    {
+        rotation.x += -Input.GetAxis("Mouse Y");
+        rotation.y += Input.GetAxis("Mouse X");
+
+        rotation.x = Mathf.Clamp(rotation.x, -15f, 15f);
+        rotation.y = Mathf.Clamp(rotation.y, -15f, 15f);
+        Camera.main.transform.localRotation = Quaternion.Euler(rotation.x * LookSpeed, rotation.y * LookSpeed, 0);
+    }
+}
