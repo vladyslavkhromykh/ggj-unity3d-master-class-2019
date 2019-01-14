3c2a0011aa983f01698938a2b636eb6d6bb38e66
Vladyslav Khromykh <hromyhvladislav@gmail.com>
2019-01-14 21:50:32 +0200

    Added scores manager

---
+++ Assets/Scripts/ReadyToUse/ScoresManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ReadyToUse
+{
+    public class ScoresManager : MonoBehaviour
+    {
+        public int Score
+        {
+            get;
+            private set;
+        }
+
+        private void Awake()
+        {
+            EventsManager.BulletHitToy += OnBulletHitToy;
+        }
+
+        private void OnBulletHitToy(Toy toy)
+        {
+            Score++;
+
+            int bestScore = PlayerPrefs.GetInt("best-score", 0);
+            if (Score > bestScore)
+            {
+                bestScore = Score;
+                PlayerPrefs.SetInt("total-score", Score);
+            }
+
+            PlayerPrefs.Save();
+        }
+    }
+}
